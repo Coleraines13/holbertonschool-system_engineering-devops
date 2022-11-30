f7713bf2d9fe577f5ead5d4f8d95c3ed9e45e90f
root <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>
2022-11-30 12:32:09 -0800

    working on it

--- 0x0E-api/1-export_to_CSV.py
+++ 0x0E-api/1-export_to_CSV.py
@@ -5,14 +5,14 @@ import sys
 import csv
 
 if __name__ == "__main__":
-        url = "https://jsonplaceholder.typicode.com/"
-        user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-        todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
-        username = user.get("username")
-        user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    username = user.get("username")
+    user_id = sys.argv[1]
 
-        with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-            writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-            [writer.writerow(
-                [user_id, username, t.get("completed"), t.get("title")]
-            ) for t in todos]
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+        ) for t in todos]
