d0e180394b18797e14ee498706c6fa1f4e128378
root <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>
2022-11-30 12:42:55 -0800

    trying for last check

--- 0x0E-api/2-export_to_JSON.py
+++ 0x0E-api/2-export_to_JSON.py
@@ -5,11 +5,11 @@ import sys
 import json
 
 if __name__ == "__main__":
+    user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
     username = user.get("username")
-    user_id = sys.argv[1]
 
     with open("{}.json".format(user_id), "w") as jsonfile:
         json.dump({user_id: [{
